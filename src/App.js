682884aa9928b45cbf8c96e1e0e4e2aff67da998
Shenran <shenran268@gmail.com>
2020-01-22 21:42:25 +0200

    remove import App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
 import Package from './components/Package';
 import Packages from './components/Packages';
 import NoMatchPage from './components/NoMatchPage'
